e989719fcb3cae3a5f92fb27eb874730fa1ffe1d
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-15 15:27:13 +0200

    stampa informazioni richieste

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// definizione array 
+string[] cibi = { "pizza", "carbonara", "sushi", "bistecca", "lasagna" };
+
+// stampo la lunghezza della classifica
+Console.WriteLine("Lunghezza della classifica. " + cibi.Length);
+
+for (int i = 0; i < cibi.Length; i++)
+{
+    // stampo ogni piatto e la sua posizione in classifica
+    Console.WriteLine("piatto in posizione "+ (i+1) + ": " + cibi[i]);
+}
+
+// stampo il primo elemento dell'array 
+Console.WriteLine("Piatto preferito: " + cibi[0]);
+
+// stampo l'ultimo elemento dell'array
+Console.WriteLine("Piatto meno preferito: " + cibi[cibi.Length -1]);
 
