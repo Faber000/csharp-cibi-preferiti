a5d6cf32fe3e064fcdc639be53155aa1fbb9c2a9
Fabio Moro <fabio.moro2000@gmail.com>
2022-09-15 15:38:17 +0200

    Bonus

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿// definizione array 
-string[] cibi = { "pizza", "carbonara", "sushi", "bistecca", "lasagna" };
+string[] cibi = {"pizza", "carbonara", "sushi", "bistecca", "lasagna"};
 
 // stampo la lunghezza della classifica
-Console.WriteLine("Lunghezza della classifica. " + cibi.Length);
+Console.WriteLine("Lunghezza della classifica: " + cibi.Length);
 
 for (int i = 0; i < cibi.Length; i++)
 {
@@ -16,3 +16,14 @@ Console.WriteLine("Piatto preferito: " + cibi[0]);
 // stampo l'ultimo elemento dell'array
 Console.WriteLine("Piatto meno preferito: " + cibi[cibi.Length -1]);
 
+
+// stampo piatti in posizione media
+if (cibi.Length % 2 == 0)
+{
+    Console.WriteLine("primo piatto in posizione media: " + cibi[(cibi.Length) / 2 -1]);
+    Console.WriteLine("secondo piatto in posizione media: " + cibi[(cibi.Length) / 2]);
+}
+else
+{
+    Console.WriteLine("piatto in posizione media: " + cibi[(cibi.Length) / 2]);
+}
